454fff07e906435e11cded09d169614aada04bb0
Aleksandr <oksanich_90@mail.ru>
2015-04-22 00:58:33 +0300

    Added js for mobile menu, mobile search and category left sidebar.

---
+++ tech-category/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('._dropdown').click(function(){
+        var $this = $(this);
+        $this.siblings().slideToggle('fast');
+        $this.children().toggleClass('arrow-down').toggleClass('arrow-up');
+    });
+
+    $('._headerSearchBtnMobile').click(function(){
+        $('._headerSearchWrap').toggleClass('hidden-xs');
+    });
+
+    $('._headerMenuBtnMobile').click(function(){
+        $('._topMenu').slideToggle('fast');
+    });
+});
