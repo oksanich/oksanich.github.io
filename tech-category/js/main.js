0c87d34c5537ee028ba79083db90e599afb50f83
Aleksandr <oksanich_90@mail.ru>
2015-05-02 23:09:10 +0300

    Added color switch script.

--- tech-category/js/main.js
+++ tech-category/js/main.js
@@ -12,4 +12,39 @@ $(document).ready(function(){
     $('._headerMenuBtnMobile').click(function(){
         $('._topMenu').slideToggle('fast');
     });
+
+    // Color switch
+
+    (function($) {
+        $.fn.removeClassWild = function(mask) {
+            return this.removeClass(function(index, cls) {
+                var re = mask.replace(/\*/g, '\\S+');
+                return (cls.match(new RegExp('\\b' + re + '', 'g')) || []).join(' ');
+            });
+        };
+    })(jQuery);
+
+    $('._topMenu ._colorElectronics').on('click', function(){
+        var wrap = '.wrapper';
+        $(wrap).removeClassWild('_*');
+        $(wrap).addClass('_colorElectronics');
+    });
+
+    $('._topMenu ._colorInternet').click(function(){
+        var wrap = '.wrapper';
+        $(wrap).removeClassWild('_*');
+        $(wrap).addClass('_colorInternet');
+    });
+
+    $('._topMenu ._colorEntertainment').click(function(){
+        var wrap = '.wrapper';
+        $(wrap).removeClassWild('_*');
+        $(wrap).addClass('_colorEntertainment');
+    });
+
+    $('._topMenu ._colorInnovation').click(function(){
+        var wrap = '.wrapper';
+        $(wrap).removeClassWild('_*');
+        $(wrap).addClass('_colorInnovation');
+    });
 });
