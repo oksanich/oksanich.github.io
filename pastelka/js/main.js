e9a974cfe3b130caa54c58e6d3509f68f8f6ca1a
Aleksandr <oksanich_90@mail.ru>
2015-08-24 21:03:33 +0300

    Modified 'catalog page'.

--- pastelka/js/main.js
+++ pastelka/js/main.js
@@ -5,3 +5,14 @@ $('#carousel').carousel({
 $('#carouselOffers').carousel({
     interval: 6000
 });
+
+$('#list').click(function(){
+    var $this = $(this);
+    $('#products .item').addClass('list-group-item');
+    $this.parent($('.catalogRightSwitch')).addClass('listItem');
+});
+$('#grid').click(function(){
+    var $this = $(this);
+    $('#products .item').removeClass('list-group-item');
+    $this.parent($('.catalogRightSwitch')).removeClass('listItem');
+});
