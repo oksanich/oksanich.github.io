7463fc6abca39e49e47b76563d8417b5cafd863d
Aleksandr <oksanich_90@mail.ru>
2015-08-25 23:40:55 +0300

    Fixed 'catalog page'.

--- pastelka/js/main.js
+++ pastelka/js/main.js
@@ -9,10 +9,10 @@ $('#carouselOffers').carousel({
 $('#list').click(function(){
     var $this = $(this);
     $('#products .item').addClass('list-group-item');
-    $this.parent($('.catalogRightSwitch')).addClass('listItem');
+    $this.parent($('.catalogRightSwitch')).removeClass('listItem');
 });
 $('#grid').click(function(){
     var $this = $(this);
     $('#products .item').removeClass('list-group-item');
-    $this.parent($('.catalogRightSwitch')).removeClass('listItem');
+    $this.parent($('.catalogRightSwitch')).addClass('listItem');
 });
